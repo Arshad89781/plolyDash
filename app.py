45d950c2c20de47ef64dc6718ab7587f98386b70
Arshad89781 <syedarshadalisha9@gmail.com>
2023-01-20 23:42:37 +0530

    "multple pages"

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = dash.Dash(__name__, use_pages=True)
 app.layout = html.Div(
     [
         # main app framework
-        html.Div("Python Multipage App with Dash", style={'fontSize':50, 'textAlign':'center'}),
+        html.Div("Python Multipages App with Dash", style={'fontSize':50, 'textAlign':'center'}),
         html.Div([
             dcc.Link(page['name']+"  |  ", href=page['path'])
             for page in dash.page_registry.values()
