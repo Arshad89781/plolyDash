52782ca23db329416d99f5086db858614f8743ee
Arshad89781 <syedarshadalisha9@gmail.com>
2023-01-20 23:29:49 +0530

    multipage

--- app.py
+++ app.py
@@ -1,48 +1,23 @@
-# import dash 
-# import dash
-from dash import dcc,Dash,html
-# import dash_core_components as dcc
-import dash_bootstrap_components as dbc  # pip install dash-bootstrap-components
-# import dash_html_components as html
+import dash
+from dash import html, dcc
 
-from dash.dependencies import Input, Output
-import plotly.express as px
+app = dash.Dash(__name__, use_pages=True)
 
-import pandas as pd
-urls = 'gapminderDataFiveYear.csv'
-df = pd.read_csv(urls)
+app.layout = html.Div(
+    [
+        # main app framework
+        html.Div("Python Multipage App with Dash", style={'fontSize':50, 'textAlign':'center'}),
+        html.Div([
+            dcc.Link(page['name']+"  |  ", href=page['path'])
+            for page in dash.page_registry.values()
+        ]),
+        html.Hr(),
 
-dash_app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
-app = dash_app.server
-
-dash_app.layout = html.Div([
-    dcc.Graph(id='graph-with-slider'),
-    dcc.Slider(
-        id='year-slider',
-        min=df['year'].min(),
-        max=df['year'].max(),
-        value=df['year'].min(),
-        marks={str(year): str(year) for year in df['year'].unique()},
-        step=None
-    )
-])
+        # content of each page
+        dash.page_container
+    ]
+)
 
 
-@dash_app.callback(
-    Output('graph-with-slider', 'figure'),
-    Input('year-slider', 'value'))
-
-def update_figure(selected_year):
-    filtered_df = df[df.year == selected_year]
-
-    fig = px.scatter(filtered_df, x="gdpPercap", y="lifeExp",
-                     size="pop", color="continent", hover_name="country",
-                     log_x=True, size_max=55, template="plotly_dark")
-
-    fig.update_layout(transition_duration=500)
-
-    return fig
-
-
-if __name__ == '__main__':
-    dash_app.run_server(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
